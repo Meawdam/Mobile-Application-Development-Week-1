068155c3ad5473b2936a8733fbfb109f63b5950a
Meawdam <meawdamxza@gmail.com>
2026-08-07 10:29:22 +0700

    looping

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,11 @@
 import 'package:main/hello.dart';
 import 'package:main/variable.dart';
-import 'package:main/input.dart';
+// import 'package:main/input.dart';
+import 'package:main/looping.dart';
 
 void main(List<String> arguments) {
   print(hello("Dart"));
   variable();
-  input();
+  // input();
+  looping();
 }
---
+++ lib/looping.dart
@@ -0,0 +1,20 @@
+void looping() {
+  //for loop
+  for (int i = 1; i < 5; i++) {
+    print(i);
+  }
+
+  //while loop
+  int j = 1;
+  while (j <= 5) {
+    print(j);
+    j++;
+  }
+
+  //dowhile loop
+  int k = 1;
+  do {
+    print(k);
+    k++;
+  } while (k <= 5);
+}
