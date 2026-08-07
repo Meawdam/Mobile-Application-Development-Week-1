beb903c76d03d84ef863e8004a0b0f90d4451389
Meawdam <meawdamxza@gmail.com>
2026-08-07 08:31:53 +0700

    refactor: restructure project by renaming and removing unused files

---
+++ bin/main.dart
@@ -0,0 +1,8 @@
+void main(List<String> arguments) {
+  String? name;
+  if(name == null) {
+    print("Unknown name");
+  } else {
+    print(name);
+  }
+}
--- bin/week_1.dart
+++
@@ -1,5 +0,0 @@
-import 'package:week_1/week_1.dart' as week_1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${week_1.calculate()}!');
-}
