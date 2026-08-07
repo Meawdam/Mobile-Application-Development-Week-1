d9370a587240edc0a3e45f538f31c9289640d1fa
Meawdam <meawdamxza@gmail.com>
2026-08-07 10:04:51 +0700

    variable and  condition statement

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,7 @@
 import 'package:main/hello.dart';
+import 'package:main/variable.dart';
 
 void main(List<String> arguments) {
   print(hello("Dart"));
+  variable();
 }
---
+++ lib/variable.dart
@@ -0,0 +1,20 @@
+void variable() {
+  // does not have to be initialized
+  // int age;
+
+  int age = 0;
+  double gpax = 0;
+  bool status = true;
+  String name = "Peter";
+
+  print('name: $name is $age years old, gpax: $gpax');
+  
+  if (status == true) {
+    print('Your status is active');
+  } else {
+    print('Your status is inactive');
+  }
+
+  //shorthnad if else statement
+  // print(status ? 'Your status is active' : 'Your status is inactive');
+}
